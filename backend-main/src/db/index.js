56cabf36bb2204b9c2708126b87d78c5693e5bc1
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-04-04 06:08:51 +0530

    backedn

---
+++ backend-main/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+
+const MONGO_URI = process.env.MONGODB_URI;
+const connectDB = async ()=>{
+    try {
+        
+        const connection =  await mongoose.connect(`${MONGO_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST : ${connection.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection error:"+error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
---
+++ backend-main/src/index.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+
+// dotenv.config({
+//     path: './env'
+// })
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+;(async()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
+        app.on("error",(error)=>{
+            console.error("Error: "+error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error: "+error)
+        throw error
+    }
+})()
+*/
