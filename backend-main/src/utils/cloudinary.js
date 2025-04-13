849085a27c974be32929a59aa1a889552592c87f
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-04-13 15:47:51 +0530

    file unsync implemented

--- backend-main/src/utils/cloudinary.js
+++ backend-main/src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) =>{
             resource_type: "auto"
         })
         // file has been uploaded successfull
-        console.log("file has been uploaded successfull on cloudinary",response.url);
+        // console.log("file has been uploaded successfull on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath)  //remove the locally saved temporaty files as the upload operation got failed
