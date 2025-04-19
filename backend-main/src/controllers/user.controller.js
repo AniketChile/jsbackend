868e767ac9a42f6a7d4da94ccc81d22a960a5a1a
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-04-19 08:50:26 +0530

    getwatchHistory endpoint

--- backend-main/src/controllers/user.controller.js
+++ backend-main/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndReferenceTokens = async (userId) => {
   try {
@@ -94,7 +95,7 @@ const registerUser = asyncHandler(async (req, res) => {
     provide the refresh token where the user is login in again or not
     send cookies
 */
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = asyncHandler(async (req, res) => { 
   const { email, username, password } = req.body;
 
   if (!username && !email) {
@@ -374,6 +375,59 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 });
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match :{
+        _id : new mongoose.Types.ObjectId(req.user._id)
+      }
+    },{
+      $lookup : {
+        from : "videos",
+        localField : "watchHistory",
+        foreignField : "_id",
+        as : "watchHistory",
+        pipeline : [
+          {
+            $lookup : {
+              from : "users",
+              localFiled :"owner",
+              foreignField : "_id",
+              as : "owner",
+              pipeline : [
+                {
+                  $project : {
+                    fullName :1,
+                    username :1,
+                    avatar :1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first : "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch History fetched Successfully"
+    )
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -385,4 +439,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
